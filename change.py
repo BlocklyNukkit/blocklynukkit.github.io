937c2a0e88adc544077bd78e6e475a518cb8acb3
helloworldSB <65109494+helloworldSB@users.noreply.github.com>
2021-02-08 12:40:24 +0800

    Update change.py

--- change.py
+++ change.py
@@ -1,7 +1,16 @@
+#! /usr/bin/env python3
+#coding:utf-8
 from pathlib import Path
 from fnmatch import fnmatch
 from re import sub
-for file in Path('.').iterdir():
+import os,os.path
+def walker(where:str,isfiler:bool = True):
+    for root,dirs,files in os.walk(r"."):
+        for file in files:
+            res = os.path.join(root,file)
+            if os.path.isfile(res) and isfiler:yield res
+            elif os.path.isdir(res) and not isfiler:yield res
+for file in walker('.'):
     if fnmatch(file,'*.md'):
         with open(file,'r',True,'utf-8') as f:buffer=f.read()
         buffer = sub(r'(?s)(\r\n|\r|\n)',r'  \1',buffer)
