c3f2e490d6c238c5ab84bb1fec0f730bd53f9a8e
超神的冰凉 <2801621420@qq.com>
2021-03-27 10:50:09 +0800

    初始化登录页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,3 +4,28 @@ const app = cloudbase.init({
 const auth = app.auth({
   persistence: "local"
 });
+//监听登录状态
+auth.onLoginStateChanged(reloadLoginStatus);
+//刷新登录状态函数
+function reloadLoginStatus(loginState){
+    if(loginState == null || loginState = undefined){
+        loginState = auth.hasLoginState();
+    }
+    if(loginState){
+        if(loginState.user != null){
+            if(loginState.user.nickName != null){
+                document.getElementById("userName").innerText = loginState.user.nickName; 
+            }else{
+                document.getElementById("userName").innerText = loginState.user.username; 
+            }
+        }else{
+            document.getElementById("userName").innerText = "未登录";
+            document.getElementById("userName").setAttribute("href","login.html"); 
+        }
+    }else{
+        document.getElementById("userName").innerText = "未登录";
+        document.getElementById("userName").setAttribute("href","login.html"); 
+    }
+}
+reloadLoginStatus();//初始化刷新
+
