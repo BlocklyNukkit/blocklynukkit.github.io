a4f6024319ab434df183e85ad087360a27c2c324
超神的冰凉 <2801621420@qq.com>
2021-04-03 15:01:28 +0800

    完善上传

--- assets/js/bnpm.js
+++ assets/js/bnpm.js
@@ -16,22 +16,87 @@ const userplugin = db.collection("userplugin");
 // }
 
 function addNewPlugin(pluginName,pluginVersion,pluginDescription,pluginInfo,pluginAssets){
+    if(pluginName == null || pluginName == undefined){
+        pluginName = $("pluginNameInput").val();
+        if(pluginName == "" || pluginName.indexOf(" ") != -1){
+            $("#uploadError").show(0).text("名称不能为空或者带有空格");return;
+        }
+    }
+    if(pluginVersion == null || pluginVersion == undefined){
+        pluginVersion = $("pluginVersionInput").val();
+        if(pluginVersion == "" || pluginVersion.indexOf(" ") != -1){
+            $("#uploadError").show(0).text("版本不能为空或者带有空格");return;
+        }
+    }
+    if(pluginDescription == null || pluginDescription == undefined){
+        pluginDescription = $("pluginDescriptionInput").val();
+    }
+    if(pluginInfo == null || pluginInfo == undefined){
+        pluginInfo = $("pluginInfoInput").val();
+    }
+    if(pluginAssets == null || pluginAssets == undefined){
+        pluginAssets = assets;
+        if(pluginAssets == null || pluginAssets.length == 0){
+            $("#uploadError").show(0).text("请先上传插件文件！");return;
+        }
+    }
     
+    var _assets;
+    isPluginExists(pluginName,function(){
+        uploadAssets(pluginName+"_"+pluginVersion,pluginAssets,function(paths){
+            _assets = paths;
+            userplugin.add({
+                name: pluginName,
+                version: pluginVersion,
+                user: auth.hasLoginState().user.email,
+                userNickName: auth.hasLoginState().user.nickName,
+                description: pluginDescription,
+                time: new Date(),
+                assets: _assets
+            })
+            .then((res) => {
+                console.log(res);
+                $("#addPluginSuccess").show(0).text(pluginName+" "+pluginVersion+"上传成功！");
+            },() => {
+                $("#uploadError").show(0).text("信息填写错误！");
+            });
+        },function(why){
+            $("#uploadError").show(0).text(why.name+"上传失败");
+        });
+    },function(){
+        $("#uploadError").show(0).text("插件重名，请更换插件名称");
+    });
 }
 
-function uploadAssets(dir,assetFiles){
-    var index = 0;
-    if(index < assetFiles.length){
-        let assetFile = assetFiles[index];
-        index++;
+function uploadAssets(dir,assetFiles,handler,failed){
+    var assetsPaths = [];
+    let assetFile = assetFiles.shift();
+    if(assetFile != undefined || assetFile != null){
         app.uploadFile({
             cloudPath: dir+"/"+assetFile.name,
             filePath: assetFile
-        })
-        .then((res) => {
+        }).then((res) => {
             console.log(res.fileID);
-            uploadAssets();
+            assetsPaths.push(res.fileID);
+            uploadAssets(dir,assetFiles);
+        },() => {
+            failed(assetFile);
         });
+    }else{
+        handler(assetsPaths);
     }
-    
+}
+
+function isPluginExists(pluginName,yes,no){
+    userplugin.where({
+        name: pluginName
+    }).get()
+    .then((res) => {
+        console.log(res.data);
+        if(res.data.length != 0){
+            no();
+        }else{
+            yes();
+        }
+    });
 }
